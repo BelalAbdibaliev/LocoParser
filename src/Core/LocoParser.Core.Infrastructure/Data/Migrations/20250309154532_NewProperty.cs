cb8f28f06836780b4f4df3489e019039cddf14f5
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-03-09 21:52:06 +0600

    Added new migration

---
+++ src/Core/LocoParser.Core.Infrastructure/Data/Migrations/20250309154532_NewProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocoParser.Core.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ItemUrl",
+                table: "Prices",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemUrl",
+                table: "Prices");
+        }
+    }
+}
